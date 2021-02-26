470c3a7676380bac98bd7e5c30d5f272a3c95479
c24b <4barbes@gmail.com>
2021-02-27 00:03:16 +0100

    Rm .venv

--- .venv/bin/ledger.py
+++
@@ -1,4 +0,0 @@
-#!/home/c24b/projects/tech_test/dataengineer/ledger/.venv/bin/python
-# EASY-INSTALL-SCRIPT: 'HledgerExercices==0.1.0','ledger.py'
-__requires__ = 'HledgerExercices==0.1.0'
-__import__('pkg_resources').run_script('HledgerExercices==0.1.0', 'ledger.py')
